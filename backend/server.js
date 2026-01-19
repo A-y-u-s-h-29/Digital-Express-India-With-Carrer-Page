48a187a375e5502683d1ed973d5ef0e74fc7c54f
Ayush Sharma <your-email@example.com>
2026-01-19 17:20:56 +0530

    backend and frontend link's update's

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://tangerine-kangaroo-546f42.netlify.app',
   credentials: true
 }));
 app.use(express.json());
